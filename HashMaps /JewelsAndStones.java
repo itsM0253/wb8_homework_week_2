d025557e4b2ad682fe2b3e3eaae8194b397d866c
itsMO253 <mwarsame34@outlook.com>
2021-02-28 19:49:15 -0800

    came up with 3 solution

--- HashMaps /JewelsAndStones.java
+++ HashMaps /JewelsAndStones.java
@@ -1,13 +1,16 @@
+import java.util.HashSet;
+
 public class JewelsAndStones {
 
 	public static void main(String[] args) {
 		String j = "aA";
 		String s = "aAAbbbb";
-		int result = jewelsStones1(j,s);
+		int result = jewelsStones2(j,s);
 		System.out.println(result);
 
 	}
 	
+	// Solution 1
 	public static int jewelsStones1(String jewels, String stones) {
 		int num = 0;
 		for(int i = 0; i < jewels.length(); i++) {
@@ -18,6 +21,35 @@ public class JewelsAndStones {
 			}
 		}
 		return num;
+	}
+	
+	// Solution 2 -> using a HashSet
+	public static int jewelsStones2(String jewels, String stones) {
+		HashSet<Character> set = new HashSet<>();
+		int count = 0;
+		for(char ch: jewels.toCharArray()) { // adding characters to the set
+			set.add(ch);
+		}
+		
+		// traverse through the different stones
+		for(char ch: stones.toCharArray()) {
+			if(set.contains(ch)) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	// Solution 3 --> using indexOf
+	public static int jewelsStones3(String jewels, String stones) {
+		int count = 0;
+		// traverse through the stones character by character
+		for(int i = 0; i < stones.length(); i++) {
+			if(jewels.indexOf(stones.charAt(i)) > -1) {
+				count++;
+			}
+		}
+		return count;
 	}
 
 
