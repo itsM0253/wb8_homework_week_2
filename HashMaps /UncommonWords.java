28be076d5198d425e371785fe8a1eb63c185e9a6
itsMO253 <mwarsame34@outlook.com>
2021-03-13 18:06:24 -0800

    Completed leetcode problem

---
+++ HashMaps /UncommonWords.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class UncommonWords {
+
+	public static void main(String[] args) {
+		String A = "this apple is sweet";
+		String B = "this apple is sour";
+		String[] answer = uncommon(A,B);
+		System.out.println(Arrays.toString(answer));
+
+	}
+	
+	public static String[] uncommon(String A, String B) {
+		HashMap<String,Integer> map = new HashMap<>();
+		String[] a = A.split(" ");
+		String[] b = B.split(" ");
+		
+		for(int i = 0; i < a.length; i++) {
+			if(!map.containsKey(a[i])) {
+				map.put(a[i], 1);
+			}
+			else {
+				map.put(a[i], map.get(a[i]) + 1);
+			}
+		}
+		
+		for(int i = 0; i < b.length; i++) {
+			if(!map.containsKey(b[i])) {
+				map.put(b[i], 1);
+			}
+			else {
+				map.put(b[i], map.get(b[i]) + 1);
+			}
+		}
+	
+		
+		int count = 0;
+		
+		// getting the size for the array
+		for(String s: map.keySet()) {
+			if(map.get(s) == 1) {
+				count++;
+			}
+		}
+		
+		// adding the values to array
+		String[] result = new String[count];
+		int index = 0;
+		for(String s: map.keySet()) {
+			if(map.get(s) == 1) {
+				result[index] = s;
+				index++;
+			}
+		}
+		return result;
+	}
+
+}
